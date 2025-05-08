7102bc56da78adf3c8d0bd2e1678f72486f06e5c
Simen Hauger Wilberg <simenhw@stud.ntnu.no>
2025-05-08 18:34:01 +0200

    Added update, delete and insert queries to product, category and brand

--- backend/constants/queries.go
+++ backend/constants/queries.go
@@ -4,12 +4,19 @@ var QueryProducts = "SELECT * FROM " + PRODUCTS_TABLE
 var QueryProduct = "SELECT * FROM " + PRODUCTS_TABLE + " WHERE " + PRODUCTID + " = ?"
 var DeleteProduct = "DELETE FROM " + PRODUCTS_TABLE + " WHERE " + PRODUCTID + " = ?"
 var InsertProduct = "INSERT INTO " + PRODUCTS_TABLE + " (" + PRODUCTID + ", " + PRODUCT_NAME + ", " + PRODUCT_DESC + ", " + PRODUCT_IMG_URL + ", " + PRODUCT_PRICE + ", " + PRODUCT_STOCK + ", " + PRODUCT_CATEGORY + ", " + PRODUCT_BRAND + ") VALUES (:" + PRODUCTID + ", :" + PRODUCT_NAME + ", :" + PRODUCT_DESC + ", :" + PRODUCT_IMG_URL + ", :" + PRODUCT_PRICE + ", :" + PRODUCT_STOCK + ", :" + PRODUCT_CATEGORY + ", :" + PRODUCT_BRAND + ")"
+var UpdateProduct = "UPDATE " + PRODUCTS_TABLE + " SET " + PRODUCT_NAME + " = :" + PRODUCT_NAME + ", " + PRODUCT_DESC + " = :" + PRODUCT_DESC + ", " + PRODUCT_IMG_URL + " = :" + PRODUCT_IMG_URL + ", " + PRODUCT_PRICE + " = :" + PRODUCT_PRICE + ", " + PRODUCT_STOCK + " = :" + PRODUCT_STOCK + ", " + PRODUCT_CATEGORY + " = :" + PRODUCT_CATEGORY + ", " + PRODUCT_BRAND + " = :" + PRODUCT_BRAND + " WHERE " + PRODUCTID + " = :" + PRODUCTID
 
 var QueryCategories = "SELECT * FROM " + CATEGORIES_TABLE
 var QueryCategory = "SELECT * FROM " + CATEGORIES_TABLE + " WHERE " + CATEGORY_NAME + " = ?"
+var InsertCategory = "INSERT INTO " + CATEGORIES_TABLE + " (" + CATEGORY_NAME + ", " + CATEGORY_DESC + ") VALUES (:" + CATEGORY_NAME + ", :" + CATEGORY_DESC + ")"
+var UpdateCategory = "UPDATE " + CATEGORIES_TABLE + " SET " + CATEGORY_DESC + " = :" + CATEGORY_DESC + " WHERE " + CATEGORY_NAME + " = :" + CATEGORY_NAME
+var DeleteCategory = "DELETE FROM " + CATEGORIES_TABLE + " WHERE " + CATEGORY_NAME + " = ?"
 
 var QueryBrands = "SELECT * FROM " + BRANDS_TABLE
 var QueryBrand = "SELECT * FROM " + BRANDS_TABLE + " WHERE " + BRAND_NAME + " = ?"
+var InsertBrand = "INSERT INTO " + BRANDS_TABLE + " (" + BRAND_NAME + ", " + BRAND_DESC + ") VALUES (:" + BRAND_NAME + ", :" + BRAND_DESC + ")"
+var UpdateBrand = "UPDATE " + BRANDS_TABLE + " SET " + BRAND_DESC + " = :" + BRAND_DESC + " WHERE " + BRAND_NAME + " = :" + BRAND_NAME
+var DeleteBrand = "DELETE FROM " + BRANDS_TABLE + " WHERE " + BRAND_NAME + " = ?"
 
 var QueryUser = "SELECT * FROM " + USERS_TABLE + " WHERE " + USERID + " = ?"
 var QueryUserLogin = "SELECT * FROM " + USERS_TABLE + " WHERE email = ?"
