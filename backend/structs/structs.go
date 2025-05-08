fb5e2a24501cb31ac7644aa61ea91e034ebcc761
Simen Hauger Wilberg <simenhw@stud.ntnu.no>
2025-05-08 22:43:35 +0200

    Added structs for patching objects

--- backend/structs/structs.go
+++ backend/structs/structs.go
@@ -36,15 +36,15 @@ type Product struct {
 	BrandName     *string `json:"brand_name" db:"Brand"`
 }
 
-type PatchProduct struct {
-	ProductID     string             `json:"product_id" db:"ProductID"`
-	Name          NullField[string]  `json:"name" db:"ProductName"`
-	Description   NullField[string]  `json:"description" db:"ProductDesc"`
-	ImgURL        NullField[string]  `json:"img_url" db:"ProductImgUrl"`
-	Price         NullField[float64] `json:"price" db:"Price"`
-	StockQuantity NullField[int]     `json:"stock_quantity" db:"StockQuantity"`
-	CategoryName  NullField[string]  `json:"category_name" db:"Category"`
-	BrandName     NullField[string]  `json:"brand_name" db:"Brand"`
+type ProductPatch struct {
+	ProductID     string            `json:"product_id" db:"ProductID"`
+	Name          string            `json:"name" db:"ProductName"`
+	Description   NullField[string] `json:"description" db:"ProductDesc"`
+	ImgURL        NullField[string] `json:"img_url" db:"ProductImgUrl"`
+	Price         float64           `json:"price" db:"Price"`
+	StockQuantity int               `json:"stock_quantity" db:"StockQuantity"`
+	CategoryName  NullField[string] `json:"category_name" db:"Category"`
+	BrandName     NullField[string] `json:"brand_name" db:"Brand"`
 }
 
 type Category struct {
@@ -67,6 +67,16 @@ type User struct {
 	Address   *string `json:"address" db:"Address"`
 }
 
+type UserPatch struct {
+	UserID    string            `json:"user_id" db:"UserID"`
+	Username  string            `json:"username" db:"Username"`
+	Password  string            `json:"password" db:"Password"` // Maybe not, yeah?
+	Email     string            `json:"email" db:"Email"`
+	FirstName string            `json:"first_name" db:"FirstName"`
+	LastName  string            `json:"last_name" db:"LastName"`
+	Address   NullField[string] `json:"address" db:"Address"`
+}
+
 type Member struct {
 	UserID          string     `json:"user_id" db:"UserID"`
 	MembershipLevel string     `json:"membership_level" db:"MembershipLevel"`
@@ -78,14 +88,29 @@ type Order struct {
 	UserID      string      `json:"user_id" db:"UserID"`
 	OrderDate   *time.Time  `json:"order_date" db:"OrderDate"`
 	OrderStatus string      `json:"order_status" db:"OrderStatus"`
-	OrderTotal  int         `json:"order_total" db:"OrderTotal"`
+	OrderTotal  float64     `json:"order_total" db:"OrderTotal"`
 	Items       []OrderItem `json:"items"`
+}
+
+type OrderPatch struct {
+	ID          string               `json:"order_id" db:"OrderID"`
+	UserID      *string              `json:"user_id" db:"UserID"`
+	OrderDate   NullField[time.Time] `json:"order_date" db:"OrderDate"`
+	OrderStatus *string              `json:"order_status" db:"OrderStatus"`
+	OrderTotal  *float64             `json:"order_total" db:"OrderTotal"`
+	Items       []OrderItem          `json:"items"`
 }
 
 type OrderItem struct {
 	OrderID   string `json:"order_id" db:"OrderID"`
 	ProductID string `json:"product_id" db:"ProductID"`
 	Quantity  int    `json:"quantity" db:"Quantity"`
+}
+
+type OrderItemPatch struct {
+	OrderID   string  `json:"order_id" db:"OrderID"`
+	ProductID *string `json:"product_id" db:"ProductID"`
+	Quantity  *int    `json:"quantity" db:"Quantity"`
 }
 
 type OrderStatus struct {
@@ -97,15 +122,24 @@ type Payment struct {
 	PaymentID     string  `json:"payment_id" db:"PaymentID"`
 	OrderID       string  `json:"order_id" db:"OrderID"`
 	PaymentMethod string  `json:"payment_method" db:"PaymentMethod"`
-	Amount        float32 `json:"amount" db:"Amount"`
+	Amount        float64 `json:"amount" db:"Amount"`
 	PaymentDate   string  `json:"payment_date" db:"PaymentDate"`
 	PaymentStatus *string `json:"payment_status" db:"PaymentStatus"`
+}
+
+type PaymentPatch struct {
+	PaymentID     string     `json:"payment_id" db:"PaymentID"`
+	OrderID       *string    `json:"order_id" db:"OrderID"`
+	PaymentMethod *string    `json:"payment_method" db:"PaymentMethod"`
+	Amount        *float64   `json:"amount" db:"Amount"`
+	PaymentDate   *time.Time `json:"payment_date" db:"PaymentDate"`
+	PaymentStatus *string    `json:"payment_status" db:"PaymentStatus"`
 }
 
 type CartItem struct {
 	UserID    string `json:"cart_id" db:"UserID"`
 	ProductID string `json:"product_id" db:"ProductID"`
-	Quantity  *int64 `json:"quantity" db:"Quantity"`
+	Quantity  *int   `json:"quantity" db:"Quantity"`
 }
 
 type Review struct {
@@ -114,4 +148,12 @@ type Review struct {
 	Comment   *string   `json:"comment" db:"Comment"`
 	Rating    *int16    `json:"rating" db:"Rating"`
 	PostDate  time.Time `json:"post_date" db:"PostDate"`
+}
+
+type ReviewPatch struct {
+	UserID    string            `json:"user_id" db:"UserID"`
+	ProductID string            `json:"product_id" db:"ProductID"`
+	Comment   NullField[string] `json:"comment" db:"Comment"`
+	Rating    NullField[int16]  `json:"rating" db:"Rating"`
+	PostDate  *time.Time        `json:"post_date" db:"PostDate"`
 }
