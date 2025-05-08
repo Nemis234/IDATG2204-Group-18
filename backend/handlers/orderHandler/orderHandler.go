84fdaa1c7c3c000159b75c5c1eb572c55a28f73a
Simen Hauger Wilberg <simenhw@stud.ntnu.no>
2025-05-08 22:23:37 +0200

    Moved payment handler to seperate file

--- backend/handlers/orderHandler/orderHandler.go
+++ backend/handlers/orderHandler/orderHandler.go
@@ -22,15 +22,9 @@ func OrderHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
 		http.Error(w, "Method not implemented", http.StatusNotImplemented)
-	default:
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-	}
-}
-
-func PaymentHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("PaymentHandler called with method: ", r.Method)
-	switch r.Method {
-	case http.MethodGet:
+	case http.MethodPut:
+		http.Error(w, "Method not implemented", http.StatusNotImplemented)
+	case http.MethodDelete:
 		http.Error(w, "Method not implemented", http.StatusNotImplemented)
 	default:
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
