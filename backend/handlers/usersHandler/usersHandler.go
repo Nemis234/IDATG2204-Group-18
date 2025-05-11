d598ff779845025ab67383a8faa64338d40c4c40
Bao Mark Ngyuen <baomn@stud.ntnu.no>
2025-05-11 22:57:03 +0200

    Working on usershandler, added role-check for GET request

--- backend/handlers/usersHandler/usersHandler.go
+++ backend/handlers/usersHandler/usersHandler.go
@@ -2,6 +2,7 @@ package usershandler
 
 import (
 	cons "backend/constants"
+	utility "backend/utility"
 	. "backend/structs"
 	"encoding/json"
 	"log"
@@ -33,7 +34,23 @@ func UsersHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("UsersHandler called with method: ", r.Method)
 	switch r.Method {
 	case http.MethodGet:
-		http.Error(w, "Method not implemented", http.StatusNotImplemented)
+
+		//Check if user is logged in with a JWT token
+		jwtTokenData, err := utility.ValidateJWT(r, cons.JwtKey)
+		if err != nil {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+			} 
+			
+		//Check if the request has admin privileges
+		if jwtTokenData.Role != "admin" {
+			http.Error(w, "Forbidden", http.StatusForbidden)
+        	return
+		}
+
+		
+
+		w.Header().Set("Content-Type", "application/json")
 	default:
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 	}
