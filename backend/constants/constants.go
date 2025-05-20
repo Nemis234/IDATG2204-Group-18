b54bf6f5ec6d8be4f99681ac42e78b94d14a4119
simenhw <simenhw@stud.ntnu.no>
2025-05-20 13:47:40 +0200

    Fixed redeclaration

--- backend/constants/constants.go
+++ backend/constants/constants.go
@@ -42,19 +42,6 @@ const (
 	ORDER_STATUS_NAME  = "StatusName"
 	ORDER_STATUS_DESC  = "StatusDesc"
 
-	ORDER_TABLE  = "OrderTable"
-	ORDER_ID     = "OrderID"
-	ORDER_DATE   = "OrderDate"
-	ORDER_STATUS = "OrderStatus"
-	ORDER_TOTAL  = "OrderTotal"
-
-	ORDER_ITEMS_TABLE = "OrderItem"
-	ORDER_QUANTITY    = "Quantity"
-
-	ORDER_STATUS_TABLE = "OrderStatus"
-	ORDER_STATUS_NAME  = "StatusName"
-	ORDER_STATUS_DESC  = "StatusDesc"
-
 	PAYMENT_TABLE  = "Payment"
 	PAYMENT_ID     = "PaymentID"
 	PAYMENT_METHOD = "PaymentMethod"
